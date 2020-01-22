5e18e45156c435d47cf8af051bb78584216de11e
dan-jugz <njugunadaniel364@gmail.com>
2020-01-22 16:51:08 +0300

    create a cart class to manage the shopping cart

---
+++ cart/cart.py
@@ -0,0 +1,20 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+from coupons.models import Coupon
+
+
+class Cart(object):
+
+    def __init__(self, request):
+        """
+        Initialize the cart.
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # save an empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
