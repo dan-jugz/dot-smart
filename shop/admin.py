9060d7ed57029503f08b0a098db1f71761851fea
dan-jugz <njugunadaniel364@gmail.com>
2020-01-22 05:50:04 +0300

    register the product and category models on the admin dashboard

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category,Product
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    
