c8f16b69c1dd5040ab52e9ce1ab6bd3fc33de475
dan-jugz <njugunadaniel364@gmail.com>
2020-01-22 17:02:39 +0300

    add a method for removing objects from cart

--- cart/cart.py
+++ cart/cart.py
@@ -34,6 +34,14 @@ class Cart(object):
             self.cart[product_id]['quantity'] += quantity
         self.save()
 
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
 
     def save(self):
         # update the session cart
