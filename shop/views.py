5a1da6094c28008e8e5e7492424d10455cdf43cc
dan-jugz <njugunadaniel364@gmail.com>
2020-01-22 06:47:23 +0300

    define a view that renders a single product and all its details

--- shop/views.py
+++ shop/views.py
@@ -15,3 +15,11 @@ def product_list(request, category_slug=None):
             'products': products}
     return render(request,'shop/product/list.html', context)
 
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,id=id,slug=slug,available=True)
+    
+    context = {'product': product}
+    return render(request,'shop/product/detail.html',context)
+
+
